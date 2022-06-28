056ab8bc4ddf493e442214a187112b43c29e4a65
brandnn07 <brandcarter4@gmail.com>
2022-06-27 20:56:40 -0400

    putting on finishing touches

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,115 @@
+// var newNoteList = document.querySelector("#title");
+// var newNotetitle = document.querySelector("#title");
+// var newNotebody = document.querySelector("#body");
+// var submitBtn = document.querySelector("#newNote-submit");
+// var deleteBtn = document.querySelector("#newNote-clear");
+// var newNotesList = document.querySelector("#newNotes-list");
+// var newNote = document.querySelector("#num-newNotes");
+
+// var makeNewNote = function() {
+//     $.ajax({
+//         url: "/api/newNote",
+//         method: "GET"
+//     }).then(function(data) {
+//         var listItems = [];
+
+//         for (var i = 0; i < data.length; i++) {
+//             var newNote = data[i];
+
+//             var li = document.querySelector("<li class='list-group-item'>").data(newNote);
+//             var row = document.querySelector("<div class='row'>");
+//             var column11 = document.querySelector("<div class='col-11'>");
+//             var newNoteBody = document.querySelector("<p>").body('"' + newNote.body + '"');
+//             var titleNote = document.querySelector("<p class='float-right'>").body("- " + newNote.title);
+//             var clearFix = document.querySelector("<div class='clearfix'>");
+//             var ratingNote = document.querySelector("<p class='float-right'>").body(newNote.rating);
+//             var column1 = document.querySelector("<div class='col-1'>");
+//             var upIcon = document.querySelector("<i class='fas fa-angle-up'>");
+//             var downIcon = document.querySelector("<i class='fas fa-angle-down'>");
+
+//             li.insertAdjacentHTML("beforeend",
+//                 row.insertAdjacentHTML("beforeend",
+//                     column11.insertAdjacentHTML("beforeend",newNoteBody, titleNote, clearFix, ratingNote),
+//                     column1.insertAdjacentHTML("beforeend",upIcon, downIcon)
+//                 )
+//             );
+
+//             listItems.push(li);
+//         }
+
+//         newNoteList.empty();
+
+//         newNoteList.insertAdjacentHTML("beforeend",listItems);
+//     });
+// };
+
+// var submitNewNote = function(event) {
+//     event.preventDefault();
+
+//     var newNote = {
+//         title: newNotetitle.value.trim(),
+//         body: newNotebody.value.trim()
+//     };
+
+//     if (!newNote.title || !newNote.body) {
+//         alert("Please fill out all the required fields!");
+//         return;
+//     }
+
+//     $.ajax({
+//             url: "/api/newNote",
+//             method: "POST",
+//             data: newNote
+//         })
+//         .then(function() {
+//             makeNewNote();
+//             newNotetitle.value;
+//             newNotebody.value;
+//         });
+// };
+
+// // function to create new notes that get populated to html using ajax GET method from apiRoutes
+// var makeNewNotes = function() {
+//     $.ajax({
+//         method: "GET",
+//         url: "/api/newNote"
+//     }).then(function(data) {
+//         var newNotesListItems = [];
+
+//         for (var i = 0; i < data.length; i++) {
+//             console.log(data[i]);
+//             var newNotes = data[i];
+//             var noteList = document.querySelector("<li class='list-group-item'>");
+//             var title = document.querySelector("<p>").text("Title: " + newNotes.title);
+//             var body = document.querySelector("<p>").text("Body: " + newNotes.body);
+//             var deleteButton = document.querySelector("<button data-noteid= " + newNotes.id + " type='button' class='delete'><span class='fa fa-trash'></button>");
+//             noteList.insertAdjacentHTML("beforeend",title, body, deleteButton);
+
+//             newNotesListItems.push(noteList);
+//         }
+
+//         newNotesList.insertAdjacentHTML("beforeend",newNotesListItems);
+//         newNote.text(newNotesListItems.length);
+//     });
+// };
+
+// makeNewNotes();
+// // delete button that deletes notes 
+// document.querySelector(document).addEventListener("click", ".delete", function() {
+//     var dataID = document.querySelector(this).data("noteid");
+//     $.ajax({
+//         url: "/api/newNote/" + dataID,
+//         type: "DELETE",
+//     }).then(
+//         function(data) {
+//             location.reload();
+//         }
+//     );
+// })
+
+// submitBtn.addEventListener("click", submitNewNote);
+
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
