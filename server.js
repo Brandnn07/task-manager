b6e39e4e6a73862c39679a1e2b6ce445fcc872e5
Brandnn07 <brandcarter4@gmail.com>
2022-06-27 20:35:37 -0400

    final touches

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
-var mysql = require("mysql");
-var express = require("express");
-var apis = require("./routes/apis");
-var public = require("./routes/public");
-
+const fs = require('fs');
+const express = require('express');
 const app = express();
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const uniqid = require('uniqid');
 
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.use(apis);
-app.use(public);
 
-
-app.listen(PORT, function() {
-  console.log(`The app is now listening at this port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`)
 });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
+
+app.get('/api/notes', function (req, res) {
+  let notesData = fs.readFileSync("./db/db.json");
+  notesData = JSON.parse(notesData);
+  res.json(notesData)
+});
+
+app.post('/api/notes', function (req, res) {
+  let notesData = fs.readFileSync("./db/db.json");
+  notesData = JSON.parse(notesData);
+  req.body.id = uniqid();
+  notesData.push(req.body);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+  res.end();
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+  let notesData = fs.readFileSync("./db/db.json");
+  let getID = notesData.findIndex(x => x.id === req.params.id);
+  notesData = JSON.parse(notesData);
+  notesData.splice(getID, 1);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+  res.end();
+})
